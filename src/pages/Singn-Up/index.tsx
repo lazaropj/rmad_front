974d83c92dc644bc43a5b586ad28aa3b6eb41fe7
Wesley <wexxcode@gmail.com>
2022-02-04 13:41:01 -0300

    feat: adding SingnUp page

---
+++ src/pages/Singn-Up/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from './styles';
+
+export const SignUp: React.FC = () => {
+  return (
+    <S.Container>
+      <h1>SignUp</h1>
+    </S.Container>
+  );
+};
