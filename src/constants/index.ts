7396f66830e63f15e95db9c9a7dc0498ffc55357
Wesley <wexxcode@gmail.com>
2022-02-04 13:39:33 -0300

    feat: adding global variables

---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+export const breakpointIphoneDevices = 320;
+export const breakpointAndroidDevices = 480;
+export const breakpointMobile = 801;
+export const breakpointLandscape = 1025;
+export const breakpointDesktop = 1281;
