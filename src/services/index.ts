eae7422b27660bcf9f4a2a4d193e856349b858cf
Wesley <wexxcode@gmail.com>
2022-02-26 11:52:10 -0300

    feat: tweaks for deploying local

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL:
+    'https://cors-anywhere.herokuapp.com/https://rmad-api.herokuapp.com/api',
 });
