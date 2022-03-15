7bcba3446cd8f572e5c989330aa43c925debdd02
Wesley <wexxcode@gmail.com>
2022-03-14 21:12:39 -0300

    feat: adjustments - DatePick

--- src/pages/CreateTravel/styles.ts
+++ src/pages/CreateTravel/styles.ts
@@ -1,17 +1,36 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  padding: 0 30px;
+
   .react-calendar {
-    background: ${props => props.theme.colors.primary};
     border-radius: 7px;
     margin-bottom: 9px;
-    border: none;
+
+    border: 2px solid #514617;
+    border-radius: 12px;
+
+    -webkit-box-shadow: 0px 1px 18px -3px rgba(0,0,0,0.75); 
+    box-shadow: 0px 1px 18px -3px rgba(0,0,0,0.75);
+
+    &__tile--now {
+      background: #f8f8f8;
+    }
+  }
+
+  .react-calendar__navigation {
+    border-bottom: 0;
+    margin-top: 1em;
+  }
+
+  .react-calendar__viewContainer {
+    border-top: 2px solid #514617;
   }
 
   .react-datetime-picker__wrapper {
     border: 2px solid #514617;
     border-radius: 30px;
-    padding: 15px;
+    padding: 10px;
   }
 
   .react-calendar__tile--active {
@@ -22,6 +41,12 @@ export const Container = styled.div`
 
   .react-datetime-picker__button:enabled:hover .react-datetime-picker__button__icon, .react-datetime-picker__button:enabled:focus .react-datetime-picker__button__icon {
     stroke: ${props => props.theme.colors.primary};
+  }
+
+  .react-calendar button {
+    margin: 0;
+    border: 0;
+    outline: none;
   }
 `;
 
@@ -43,20 +68,27 @@ export const FormContainer = styled.div`
 `;
 
 export const InputsContainer = styled.div`
-    input {
+
+  input {
     border-radius: 50px;
     overflow: hidden;
-    background: #514617;
-    color: ${({ theme }) => theme.colors.white.base};
+    background: #f8f8f8;
+    border-color: #ffffff;
+    color: ${({ theme }) => theme.colors.black[100]};
     font-size: 0.8rem;
     padding: 0 30px;
+    height: 56px;
 
     :focus {
-      background: #514617;
-      color: white;
+      background: #f8f8f8;
+      color: ${({ theme }) => theme.colors.black[100]};
+      border: 2px solid ${({ theme }) => theme.colors.black[100]};
+      box-shadow: 0 0 0 0.3rem rgb(255 212 40);
+      font-size: 1rem;
+      font-weight: 400;
 
       label {
-        color: ${({ theme }) => theme.colors.primary};
+        color: ${({ theme }) => theme.colors.black[100]};
       }
     }
 
@@ -65,45 +97,57 @@ export const InputsContainer = styled.div`
     }
     &:focus + label,
     &:not(:placeholder-shown) + label {
-      margin-top: 5px;
+      margin-top: 12px;
       font-size: 0.8rem;
       line-height: 1.5rem;
 
       top: calc(50% - 29px);
-      color: white;
+      color: ${({ theme }) => theme.colors.black[100]};
     }
   }
 
   label:first-child {
     padding: 0;
     font-size: 0.8rem;
+    display: none;
   }
 
   label {
     padding: 30px;
     font-size: 0.8rem;
+    color: ${({ theme }) => theme.colors.black[100]};
   }
 `;
 
 export const DatePickerContainer = styled.div`
   display: flex;
   flex-direction: column;
+  margin-bottom: 15px;
 
   label {
     font-size: 0.8rem;
+    text-align: center;
   }
 `;
 
 export const ButtonContainer = styled.div`
-    button {
-    background: ${({ theme }) => theme.colors.primary};
-    color: ${({ theme }) => theme.colors.black[100]};
-    width: 100%;
-    border: 2px solid ${({ theme }) => theme.colors.black[100]};
+  display: flex;
+  gap: 12px;
 
-    margin-top: 30px;
+  button {
+  background: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.black[100]};
+  width: 100%;
+  border: none;
+
+  margin-top: 30px;
 
     &:hover {
+      background: ${({ theme }) => theme.colors.black[100]};
+      color: ${({ theme }) => theme.colors.white.base};
+    }
+
+    &:first-child {
       background: ${({ theme }) => theme.colors.black[100]};
       color: ${({ theme }) => theme.colors.white.base};
     }
